e249b38b77035ce896138b97d18a7667c8c2bbc5
fangyi <408067643@qq.com>
2018-07-27 16:50:08 +0800

    去掉插入数据时的_class字段

--- src/main/java/org/great/TestApplication.java
+++ src/main/java/org/great/TestApplication.java
@@ -1,10 +1,30 @@
 package org.great;
 
+import com.mongodb.Mongo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.MongoDbFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.convert.DefaultDbRefResolver;
+import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
 
 @SpringBootApplication
 public class TestApplication {
+
+
+    /**
+     * 去除插入时生成的_class字段
+     * @param mongoDbFactory mongoDbFactory
+     * @return
+     */
+    @Bean
+    public MongoTemplate mongoTemplate(MongoDbFactory mongoDbFactory, MappingMongoConverter converter) {
+        converter.setTypeMapper(new DefaultMongoTypeMapper(null));
+        return new MongoTemplate(mongoDbFactory, converter);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(TestApplication.class, args);
--- src/main/java/org/great/domain/Company.java
+++ src/main/java/org/great/domain/Company.java
@@ -1,15 +1,16 @@
 package org.great.domain;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import java.sql.Timestamp;
+import java.util.Date;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.Field;
 
-import java.sql.Timestamp;
 
 @Getter
 @Setter
@@ -21,12 +22,10 @@ public class Company {
     @Id
     private String id;
 
-    @Field(value = "companyCode")
     private String code;
 
-    @Field(value = "companyName")
     private String name;
 
-//    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
-//    private Timestamp createTime;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Date createDate;
 }
